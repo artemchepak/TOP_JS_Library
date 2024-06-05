f3b01778912992727ea932d2af46fc19090bc12e
Artem Chepak <a.chepak91@gmail.com>
2024-06-05 16:24:21 -0600

    add inserting data into table from the library array

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const table = document.querySelector('#table-body');
 
 function Book(title, author, pages, status) {
     this.title = title;
@@ -15,3 +16,13 @@ function addBookToLibrary() {
 }
 
 let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+let theHobbit2 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+let theHobbit3 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
+
+
+myLibrary.push(theHobbit, theHobbit2, theHobbit3);
+
+myLibrary.forEach(element => {
+    let newRow = table.insertRow();
+    newRow.innerHTML = `<th scope="row">${myLibrary.indexOf(element) + 1}</th><td>${element.title}</td><td>${element.author}</td><td>${element.pages}</td><td>${element.status}</td>`;
+});
