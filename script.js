59949d550aaa096cdbecfeb94ce4c06940f1cd75
Artem Chepak <a.chepak91@gmail.com>
2024-06-06 11:21:55 -0600

    add adding book and updating table functions

--- script.js
+++ script.js
@@ -8,27 +8,27 @@ function Book(title, author, pages, status) {
     this.author = author;
     this.pages = pages;
     this.status = status;
-    this.info = function() {
+    this.info = function () {
         return `${this.title} by ${this.title}, ${this.pages} pages, ${this.status}`;
     }
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let status = document.querySelector('#status').value;
+    let newBook = new Book(title, author, pages, status);
+    myLibrary.push(newBook);
+    updateTable();
+};
+
+function updateTable() {
+    let newRow = table.insertRow();
+    let element = myLibrary.slice(-1)[0];
+    newRow.innerHTML = `<th scope="row">${myLibrary.indexOf(element) + 1}</th><td>${element.title}</td><td>${element.author}</td><td>${element.pages}</td><td>${element.status}</td>`;
 }
 
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-let theHobbit2 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-let theHobbit3 = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-
-
-myLibrary.push(theHobbit, theHobbit2, theHobbit3);
-
-myLibrary.forEach(element => {
-    let newRow = table.insertRow();
-    newRow.innerHTML = `<th scope="row">${myLibrary.indexOf(element) + 1}</th><td>${element.title}</td><td>${element.author}</td><td>${element.pages}</td><td>${element.status}</td>`;
+btn.addEventListener('click', function () {
+    addBookToLibrary();
 });
-
-// btn.addEventListener('click', function(){
-//     bookModal.style.display = 'none';
-// });
